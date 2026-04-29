b078cb06259689841f2de6b7212ce4e515fef524
강세환(플밍4기) <ghks5658@gmail.com>
2026-04-29 16:39:53 +0900

    탄퍼짐 시스템 구현

--- Assets/_Project/Scripts/Actors/Player/WeaponSubsystems.cs
+++ Assets/_Project/Scripts/Actors/Player/WeaponSubsystems.cs
@@ -24,26 +24,4 @@ namespace DeadZone.Actors
             isReloading = false;
         }
     }
-
-    public class ADSSystem : NetworkBehaviour
-    {
-        public bool IsADS { get; private set; }
-        public void SetADS(bool aiming) { if (IsOwner) IsADS = aiming; }
-    }
-
-    public class WeaponSwitching : NetworkBehaviour
-    {
-        private EquipmentSlots equipment;
-
-        private void Awake()
-        {
-            equipment = GetComponent<EquipmentSlots>();
-        }
-
-        public void RequestEquip(WeaponSlot slot)
-        {
-            if (!IsOwner || equipment == null) return;
-            equipment.SwitchToServerRpc(slot);
-        }
-    }
 }
